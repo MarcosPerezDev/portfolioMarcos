152eb87d09fdb681f9da3158970c0fd3465ce968
MarcosPerezDev <marcos.perez.developer@gmail.com>
2024-02-28 13:00:23 +0100

    navbar

---
+++ components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import "./styles.scss"
+
+const Navbar=()=>{
+    return (
+        <div className="contenedor">
+            
+            <ul className="navbar">
+                <li>Mi CV</li>
+                <li>MIS PROYECTOS</li>
+                <li>SOBRE MI</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
